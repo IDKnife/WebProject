4a0331e7ee6bbc21bd86b3b57c284cb47615488f
IDKnife <IvanovStas121@yandex.ru>
2020-08-24 13:11:01 +0300

    Added price of order to order list

--- Frontend/ClientApp/src/components/ClientOrders.js
+++ Frontend/ClientApp/src/components/ClientOrders.js
@@ -8,6 +8,7 @@ export class ClientOrders extends Component {
     constructor(props) {
         super(props);
         this.getContent = this.getContent.bind(this);
+        this.getPrice = this.getPrice.bind(this);
         this.state = {
             client: {},
             IsLoaded: false
@@ -15,6 +16,8 @@ export class ClientOrders extends Component {
     }
 
     getContent(e) {
+        if (e.target.parentNode.id === "")
+            return;
         Array.from(e.target.parentNode.parentNode.childNodes).map(a => {
             a.style.border = "none";
             a.style.padding = "5px";
@@ -22,13 +25,24 @@ export class ClientOrders extends Component {
         e.target.parentNode.style.border = "3px solid black";
         e.target.parentNode.style.padding = "2px";
         let elem = document.getElementById("orderContent");
-        elem.innerHTML = "<div><p>Product</p><p>Price</p><p>Count</p></div>";
+        elem.innerHTML = "<div><p><b>Product</b></p><p><b>Price</b></p><p><b>Count</b></p></div>";
         let order = this.state.client.orders.find(a => a.id === e.target.parentNode.id);
         order.basket.products.map(a => {
             elem.innerHTML += ("<div><p>" + a.product.name + "</p>"
                 + "<p>" + a.product.price + "</p>"
                 + "<p>" + a.count + "</p></div>");
         });
+    }
+
+    async getPrice(id) {
+        let price;
+        await axios.get(`https://localhost:5001/api/Order/GetPriceOfOrder/${id}`)
+            .then(res => res.data)
+            .then((data) => {
+                price = data;
+            })
+            .catch(console.log);
+        document.getElementById(id).innerHTML += `<td class="price">${price}</td>`;
     }
 
     async componentDidMount() {
@@ -58,6 +72,7 @@ export class ClientOrders extends Component {
                             <th>Id</th>
                             <th>State</th>
                             <th>Date</th>
+                            <th class="price">Price</th>
                         </tr>
                         {client.orders.map((order) => {
                             let state;
@@ -75,6 +90,7 @@ export class ClientOrders extends Component {
                                     state = "Delievered";
                                     break;
                             }
+                            this.getPrice(order.id);
                             return (
                                 <tr id={order.id} onClick={this.getContent}>
                                     <td>{order.id}</td>
